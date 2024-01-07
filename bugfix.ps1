aece505061bf53aba14e2a282ac12e18e3c0f926
Sorna Murugan <L00179495@atu.ie>
2024-01-07 20:48:15 +0000

    bug fix code added in bugfix.ps1 file and merged to main branch

---
+++ bugfix.ps1
@@ -0,0 +1,13 @@
+# Create a hash table
+$person = @{}
+
+# Add key-value pairs to the hash table
+$person["Name"] = "John"
+$person["Age"] = 25
+$person["City"] = "New York"
+
+# Looping through the hash table
+foreach ($key in $person.Keys) {
+   
+      Write-Host $key,$person[$key]
+}
